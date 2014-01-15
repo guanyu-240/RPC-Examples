8cdb9e5e24f70778b861564639d200167dd72ced
Guanyu Wang <garrett@linux-1olp.site>
2014-01-15 00:22:46 -0500

    Code update

--- File-Transfer/file.x
+++ File-Transfer/file.x
@@ -1,37 +1,58 @@
+/*
+ * Limits of file name length and chunk size.
+ */
 const MAX_CHUNK_SIZE = 4096;
 const MAX_FNAME_LEN = 1024;
 
 typedef int boolean;
 
+/*
+ * Definition of filename type.
+ */
 typedef opaque filename[MAX_FNAME_LEN];
 
+/*
+ * Definition of Transfer request data structure
+ */
 struct request{
-	filename f_name;
-	u_long offset;
+    filename f_name;
+    u_long offset;
 };
 
 typedef struct request request;
 
+/*
+ * Definition of 'chunk' type for storing bytes of the file chunks
+ */
 typedef opaque chunk[MAX_CHUNK_SIZE];
 
+/*
+ * Definition of data structure for sending file chunks
+ */
 struct filechunk {
-	chunk data;
-	u_long offset;
-	u_long bytes;
-	boolean end;
+    chunk data;
+    u_long offset;
+    u_long bytes;
+    boolean end;
 };
 
 typedef struct filechunk filechunk;
 
+/*
+ * Definition of data structure for return of procedure
+ */
 union get_file_ret switch (int errno) {
-	case 0:
-		filechunk f_chunk;
-	default:
-		void;
+    case 0:
+        filechunk f_chunk;
+    default:
+        void;
 };
 
+/*
+ * Definition of the procedure
+ */
 program FT{
-	version FTVERS {
-		get_file_ret get_file(request *) = 1;
-	} = 1;
+    version FTVERS {
+        get_file_ret get_file(request *) = 1;
+    } = 1;
 } = 0x20000011;
--- File-Transfer/rpc_ft_client.c
+++ File-Transfer/rpc_ft_client.c
@@ -9,73 +9,81 @@ extern __thread int errno;
 
 FILE *fp;
 CLIENT *clnt;
+
 void init_fp(char *f_name) {
-	fp = fopen(f_name, "wb");
-	if (fp == NULL) {
-		printf("Initialize file pointer error!\n");
-		exit(1);
-	}
+    fp = fopen(f_name, "wb");
+    if (fp == NULL) {
+        printf("Initialize file pointer error!\n");
+        exit(1);
+    }
 }
 
 void init_clnt(char *host) {
-	clnt = clnt_create(host, FT, FTVERS, "udp");
-	if (clnt == NULL) {
-		clnt_pcreateerror(host);
-		fclose(fp);
-		exit(1);
-	}
+    clnt = clnt_create(host, FT, FTVERS, "udp");
+    if (clnt == NULL) {
+        clnt_pcreateerror(host);
+        fclose(fp);
+        exit(1);
+    }
 }
 
+/*
+ * Call the remote procedure and get the file
+ */
 int get_file_rpc(char *host, char *f_name) {
-	init_fp(f_name);
-	init_clnt(host);
+    init_fp(f_name);
+    init_clnt(host);
 
-	size_t offset = 0;
-	size_t written = 0;
-	get_file_ret *ret;
-	request req;
-	memset(req.f_name, 0, MAX_FNAME_LEN);
-	memcpy(req.f_name, f_name, (strlen(f_name) <=  MAX_FNAME_LEN ? strlen(f_name): MAX_FNAME_LEN));
-	while (1) {
-		req.offset = offset;
-		ret = get_file_1(&req, clnt);
-		if (ret == NULL) {
-			clnt_perror(clnt, host);
-			fclose(fp);
-			exit(1);
-		}
+    size_t offset = 0;
+    size_t written = 0;
+    get_file_ret *ret;
+    request req;
+    memset(req.f_name, 0, MAX_FNAME_LEN);
+    memcpy(req.f_name, f_name, (strlen(f_name) <=  MAX_FNAME_LEN ? strlen(f_name): MAX_FNAME_LEN));
+    while (1) {
+        req.offset = offset;
+        ret = get_file_1(&req, clnt);
 
-		if (ret->errno != 0) {
-			errno = ret->errno;
-			perror(f_name);
-			fclose(fp);
-			exit(1);
-		}
+        // connection error
+        if (ret == NULL) {
+            clnt_perror(clnt, host);
+            fclose(fp);
+            exit(1);
+        }
 
-		// successfully got a file chunk
-		if (ret->get_file_ret_u.f_chunk.offset == offset) {
-			printf("Write a chunk\n");
-			written = fwrite(ret->get_file_ret_u.f_chunk.data, 1, 
-						ret->get_file_ret_u.f_chunk.bytes, fp);
-			if (written < 0) {
-				perror("Error in writing\n");
-				exit(1);
-			}
-			offset += ret->get_file_ret_u.f_chunk.bytes;
-		}
-		if (ret->get_file_ret_u.f_chunk.end) break;
-	}
-	
-	fclose(fp);
-	return 0;
+        // file name does not exist
+        if (ret->errno != 0) {
+            errno = ret->errno;
+            perror(f_name);
+            fclose(fp);
+            exit(1);
+        }
+
+        // successfully got a file chunk
+        if (ret->get_file_ret_u.f_chunk.offset == offset) {
+            printf("Write a chunk\n");
+            written = fwrite(ret->get_file_ret_u.f_chunk.data, 1,
+                        ret->get_file_ret_u.f_chunk.bytes, fp);
+            if (written < 0) {
+                perror("Error in writing\n");
+                exit(1);
+            }
+            offset += ret->get_file_ret_u.f_chunk.bytes;
+        }
+        if (ret->get_file_ret_u.f_chunk.end) break;
+    }
+
+    fclose(fp);
+    return 0;
 }
 
 int main(int argc, char **argv) {
-	if (argc != 3) {
-		printf("Usage: ./rpc_ft_client <host> <file>\n");
-		exit(1);
-	}
-	printf("%s : %lu\n", argv[2], strlen(argv[2]));
-	get_file_rpc(argv[1], argv[2]);
-	return 0;
+    if (argc != 3) {
+        printf("Usage: ./rpc_ft_client <host> <file>\n");
+        exit(1);
+    }
+    printf("%s : %lu\n", argv[2], strlen(argv[2]));
+    get_file_rpc(argv[1], argv[2]);
+    return 0;
 }
+
